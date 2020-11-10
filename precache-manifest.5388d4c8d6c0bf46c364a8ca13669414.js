412de7a6ae0c5c668a78bd734c511e1392a46c3e
AntonChetveryk <chetverykanton92@gmail.com>
2020-11-10 20:46:56 +0200

    Updates

---
+++ precache-manifest.5388d4c8d6c0bf46c364a8ca13669414.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99036a75ef1488afb16ef4a9609a8bea",
+    "url": "/react_movies-list-add-form/index.html"
+  },
+  {
+    "revision": "090fbe92bddb148e4b40",
+    "url": "/react_movies-list-add-form/static/css/2.92730f33.chunk.css"
+  },
+  {
+    "revision": "38194ae34a9f7913e6f1",
+    "url": "/react_movies-list-add-form/static/css/main.9f3ef87a.chunk.css"
+  },
+  {
+    "revision": "090fbe92bddb148e4b40",
+    "url": "/react_movies-list-add-form/static/js/2.f7c907f7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react_movies-list-add-form/static/js/2.f7c907f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38194ae34a9f7913e6f1",
+    "url": "/react_movies-list-add-form/static/js/main.71abcd8a.chunk.js"
+  },
+  {
+    "revision": "ed0df1cf73b7c0697c28",
+    "url": "/react_movies-list-add-form/static/js/runtime-main.128e3671.js"
+  }
+]);
